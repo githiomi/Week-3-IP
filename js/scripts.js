250dd1da63971c3d8181b50207df29a322663e81
Dhosio <danielgithiomi@gmail.com>
2020-02-21 15:47:26 +0300

    spaced portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -12,24 +12,22 @@ $(document).ready(function(){
 
 		$("#output").show();
 		alert("Thank you " + name + " for your feedback. We will be in touch via " +email);
-
+	
 	});
 
 	$("#shift1").click(function(){
-		$(".img1").slideToggle(1500);
-		$(".txt1").slideToggle(1500);
+		$(".img1").slideToggle(1600);
+		$(".txt1").slideToggle(1600);
 	});
 
 	$("#shift2").click(function(){
-		$(".img2").slideToggle(1500);
-		$(".txt2").slideToggle(1500);
+		$(".img2").slideToggle(1600);
+		$(".txt2").slideToggle(1600);
 	});
 
 	$("#shift3").click(function(){
-		$(".img3").slideToggle(1500);
-		$(".txt3").slideToggle(1500);
+		$(".img3").slideToggle(1600);
+		$(".txt3").slideToggle(1600);
 	});
-
-
 
 });
